fe932165d56bffe11aa091f21dc83002dd6de23b
Denise Deng <siktang1221@gmail.com>
2024-05-29 14:00:03 -0400

    fixed slider function

--- main.js
+++ main.js
@@ -4,15 +4,8 @@ function updateSlider(slider, output) {
     });
 }
 
-const yearSlider = document.getElementById("year");
-const yearValue = document.getElementById("yearValue");
-
 const ratingSlider = document.getElementById("rating");
 const ratingValue = document.getElementById("ratingValue");
-
-
-
-updateSlider(yearSlider, yearValue);
 
 updateSlider(ratingSlider, ratingValue);
 
